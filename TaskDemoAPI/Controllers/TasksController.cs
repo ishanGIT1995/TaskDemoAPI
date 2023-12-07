9737536d8e2b5a3424f766cb34b865688eb290a8
ishanGIT1995 <ishanrandika727@gmail.com>
2023-12-07 16:40:52 +0530

    Add project files.

---
+++ TaskDemoAPI/Controllers/TasksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskDemoAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        [HttpGet]
+        public List<string> GetAllTasks()
+        {
+            return new List<string>() { "Go Shopping", "Do homework", "Morning meeting"};
+
+        }
+    }
+}
