0c415fb412a117d72b7864f3395ef7cb0ab3d900
ishanGIT1995 <ishanrandika727@gmail.com>
2023-12-08 01:33:58 +0530

    Added Models folder and created Task model class

---
+++ TaskDemoAPI/Models/Task.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskDemoAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; } 
+    }
+}
